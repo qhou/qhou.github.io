6b68146c80eb29e3d5f4cb373635246868e769db
QuinnHou <queiyun.hou@gmail.com>
2017-12-25 19:51:51 -0500

    changed modal show function

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('slideshow');
+  //after last page, go back to page 1
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  else if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+}
+
+function toggleModal(id){
+  $(id).toggle();
+}
+
